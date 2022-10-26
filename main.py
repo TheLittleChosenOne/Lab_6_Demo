48a49217afdbca0246c1593c7c18be3321bee49e
Erik Mercado <eriksalomonmercado@gmail.com>
2022-10-26 00:33:24 -0400

    Add code

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
-def add(a, b):
-	return a + b
 
-if __name__ == "__main__":
-	print(add(1, 4))
+first_num = float(input("Enter first operand: "))
+second_num = float(input("Enter second operand: "))
+
+print("Calculator Menu\n"
+          "---------------\n"
+          "1. Addition\n"
+          "2. Subtraction\n"
+            )
+
+
+operation_num = int(input("Which operation do you want to perform? "))
+
+if (operation_num == 1):
+    result = (first_num + second_num)
+
+elif (operation_num == 2):
+    result = (first_num - second_num)
+
+else:
+    print("Error: Invalid selection! Terminating program.")
+
+print(f"The result of the operation is  { result } . Goodbye!")
+
+
+
+
